2ee90e7eef5b6f1933e5f0f922e8d3b86d0fd9bf
Ravi <ravi@C-BL-WV-LT-0138.local>
2025-05-19 11:52:48 +0530

    seo fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,44 @@ import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 import { Analytics } from "@vercel/analytics/react"
 
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Hope Foundation - Empowering Lives Through Education',
-  description: 'We are dedicated to supporting education, providing food assistance, and helping students achieve their dreams.',
+  title: 'Aasha Ki Ek Kiran - Empowering Lives Through Education & Compassion',
+  description: 'Aasha Ki Ek Kiran is a nonprofit organization dedicated to educating underprivileged children, supporting elders, and empowering women through impactful community initiatives.',
+  keywords: [
+    'NGO India',
+    'Nonprofit Education',
+    'Donate to NGO',
+    'Support Underprivileged Children',
+    'Aasha Ki Ek Kiran',
+    'Women Empowerment NGO',
+    'Old Age Home Support',
+    'Child Welfare',
+    'Volunteer NGO',
+  ],
+  icons: {
+    icon: '/favicon.ico',
+  },
+
+  openGraph: {
+    title: 'Aasha Ki Ek Kiran - Empowering Lives Through Education & Compassion',
+    description:
+      'Join Aasha Ki Ek Kiran in transforming lives through education, donations, and social impact events. Support today and be the change.',
+    url: 'https://www.aashakiekkiran.org',
+    siteName: 'Aasha Ki Ek Kiran',
+    locale: 'en_IN',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Aasha Ki Ek Kiran - Empowering Lives',
+    description:
+      'We support education, food relief, women empowerment, and social welfare. Help us build a better tomorrow.', // Update if needed
+  },
 };
+
 
 export default function RootLayout({
   children,
@@ -20,6 +52,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+      <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`${inter.className} bg-white`}>
         <ThemeProvider
           attribute="class"
